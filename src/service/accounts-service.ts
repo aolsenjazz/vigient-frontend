84e6496de24c419f2c531fd8543b4386d280977d
Alexander Olsen <aolsenjazz@gmail.com>
2023-08-29 12:14:32 -0400

    pull latest backend

--- src/service/accounts-service.ts
+++ src/service/accounts-service.ts
@@ -1,40 +1,40 @@
-import { AccountDAO, AccountDAOImpl } from "@domain/account-dto";
+import { AccountDTO, AccountDTOImpl } from "@domain/dto/accounts-dto";
 import { client } from "../vigient-client";
 
 class AccountsService {
   static async getAllAccounts(
     limit: number,
     offset: number,
-  ): Promise<AccountDAO[]> {
+  ): Promise<AccountDTO[]> {
     const response = await client.get("/accounts", {
       params: { limit, offset },
     });
 
     return response.data.map(
-      (account: AccountDAO) => new AccountDAOImpl(account),
+      (account: AccountDTO) => new AccountDTOImpl(account),
     );
   }
 
-  static async getAccountById(id: number): Promise<AccountDAO> {
+  static async getAccountById(id: number): Promise<AccountDTO> {
     const response = await client.get(`/accounts/${id}`);
 
-    return new AccountDAOImpl(response.data);
+    return new AccountDTOImpl(response.data);
   }
 
-  static async createAccount(handle: string): Promise<AccountDAO> {
+  static async createAccount(handle: string): Promise<AccountDTO> {
     const response = await client.post("/accounts", {
       handle,
     });
 
-    return new AccountDAOImpl(response.data);
+    return new AccountDTOImpl(response.data);
   }
 
-  static async updateAccount(id: number, handle: string): Promise<AccountDAO> {
+  static async updateAccount(id: number, handle: string): Promise<AccountDTO> {
     const response = await client.put(`/accounts/${id}`, {
       handle,
     });
 
-    return new AccountDAOImpl(response.data);
+    return new AccountDTOImpl(response.data);
   }
 
   static async deleteAccount(id: number): Promise<void> {
