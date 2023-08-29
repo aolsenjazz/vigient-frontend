c355c0353959dbb31f210ceb48e2082954f609a0
Alexander Olsen <aolsenjazz@gmail.com>
2023-08-29 12:09:40 -0400

    update backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import './styles/App.css'; // Import the main stylesheet for your entire application
+import React, { useState } from "react";
+import "./styles/App.css"; // Import the main stylesheet for your entire application
 
 // Import your components for each "page"
-import Main from './components/Main';
-import Accounts from './components/Accounts';
-import Sources from './components/Sources';
-import Jobs from './components/Jobs';
-import Navbar from './components/Navbar'; // Import the Navbar component
-import Footer from './components/Footer'; // Import the Footer component
+import Main from "./components/Main";
+import Accounts from "./components/Accounts";
+import Sources from "./components/Sources";
+import Jobs from "./components/Jobs";
+import Navbar from "./components/Navbar"; // Import the Navbar component
+import Footer from "./components/Footer"; // Import the Footer component
 
 function App() {
   // State to manage the current active page
-  const [currentPage, setCurrentPage] = useState<string>('main');
+  const [currentPage, setCurrentPage] = useState<string>("main");
 
   // Function to change the current page
   const changePage = (page: string) => {
@@ -21,13 +21,13 @@ function App() {
   // Render the appropriate component based on the current page
   const renderPage = () => {
     switch (currentPage) {
-      case 'main':
+      case "main":
         return <Main />;
-      case 'accounts':
+      case "accounts":
         return <Accounts />;
-      case 'sources':
+      case "sources":
         return <Sources />;
-      case 'jobs':
+      case "jobs":
         return <Jobs />;
       default:
         return <Main />; // Default to the main page
@@ -40,9 +40,7 @@ function App() {
       <Navbar changePage={changePage} />
 
       {/* Main Content Area */}
-      <div className="main-content">
-        {renderPage()}
-      </div>
+      <div className="main-content">{renderPage()}</div>
 
       {/* Footer */}
       <Footer />
